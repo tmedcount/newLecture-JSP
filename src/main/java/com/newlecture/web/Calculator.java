1af841a79e4f192503f3a4cba745f0a9bbb5e796
yamto <tmedcount@gmail.com>
2021-10-27 15:39:23 +0900

    계산기 프로그램 하나의 서블릿으로 합치기

--- src/main/java/com/newlecture/web/Calculator.java
+++ src/main/java/com/newlecture/web/Calculator.java
@@ -1,37 +1,156 @@
 package com.newlecture.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/calculator")
 public class Calculator extends HttpServlet {
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		// get, post 한 번에 처리 필요. => super.service() override는 삭제 + get, post ovrride도 삭제.
-		if(req.getMethod().equals("GET")) {
-			System.out.println("GET요청이 왔습니다.");
-		} else if(req.getMethod().equals("POST")) {
-			System.out.println("POST요청이 왔습니다.");
-		}
-		
-		super.service(req, resp);
-		
-	}
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		
+//		// get, post 한 번에 처리 필요. => super.service() override는 삭제 + get, post ovrride도 삭제.
+//		if(req.getMethod().equals("GET")) {
+//			System.out.println("GET요청이 왔습니다.");
+//		} else if(req.getMethod().equals("POST")) {
+//			System.out.println("POST요청이 왔습니다.");
+//		}
+//		
+//		super.service(req, resp);
+//		
+//	}
 	
 	// get, post 따로 처리. => service() override 삭제.
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doGet 메소드가 호출 되었습니다.");
+		Cookie[] cookies = req.getCookies();
+		
+		String exp = "0";
+		if(cookies != null) {
+			for(Cookie c : cookies) {
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}	
+			}
+		}
+			
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = resp.getWriter(); 
+				
+		out.write("<!DOCTYPE html>");
+		out.write("<html>");
+		out.write("<head>");
+		out.write("<meta charset=\"EUC-KR\">");
+		out.write("<title>Insert title here</title>");
+		out.write("<style>");
+		out.write("input {");
+		out.write("width: 50px;");
+		out.write("height: 50px;");
+		out.write("}");
+			
+		out.write(".output {");
+		out.write("height: 50px;");
+		out.write("background: #e9e9e9;");
+		out.write("font-size: 24px;");
+		out.write("font-weight: bold;");
+		out.write("text-align: right;");
+		out.write("padding: 0px 5px;");
+		out.write("}");
+		out.write("</style>");
+		out.write("</head>");
+		out.write("<body>");
+		out.write("<form method=\"post\" accept-charset=\"utf-8\">");
+		out.write("<table>");
+		out.write("<tr>");
+		out.printf("<td class=\"output\" colspan=\"4\">%s</td>", exp);
+		out.write("</tr>");
+		out.write("<tr>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"CE\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"C\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"BS\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"/\" /></td>");
+		out.write("</tr>");
+		out.write("<tr>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"7\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"8\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"9\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"*\" /></td>");
+		out.write("</tr>");
+		out.write("<tr>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"4\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"5\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"6\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"-\" /></td>");
+		out.write("</tr>");
+		out.write("<tr>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"1\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"2\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"3\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"+\" /></td>");
+		out.write("</tr>");
+		out.write("<tr>");
+		out.write("<td></td>");
+		out.write("<td><input type=\"submit\" name=\"value\" value=\"0\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"dot\" value=\".\" /></td>");
+		out.write("<td><input type=\"submit\" name=\"operator\" value=\"=\" /></td>");
+		out.write("</tr>");
+		out.write("</table>");
+		out.write("</form>");
+		out.write("</body>");
+		out.write("</html>");
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doPost 메소드가 호출 되었습니다.");
+		Cookie[] cookies = req.getCookies(); // WebBrowser별 path 범주 공간
+		
+		String value = req.getParameter("value");
+		String operator = req.getParameter("operator");
+		String dot = req.getParameter("dot");
+		
+		String exp = "";
+		if(cookies != null)
+			for(Cookie c : cookies) {
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}	
+			}
+		
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(operator != null && operator.equals("C")) { 
+			exp = "";
+		} else {
+			exp += (value == null)?"":value;
+			exp += (operator == null)?"":operator;
+			exp += (dot == null)?"":dot;
+		}
+		
+		Cookie expCookie = new Cookie("exp", exp);
+		
+		if(operator != null && operator.equals("C"))
+			expCookie.setMaxAge(0);
+		
+		expCookie.setPath("/calculator");
+		resp.addCookie(expCookie);
+		resp.sendRedirect("calculator"); // redirection
 	}
 }
