fa9663b9a4239012c70d77b86dfb305fce3296ea
yamto <tmedcount@gmail.com>
2021-10-27 10:54:00 +0900

    동적인 페이지(서버 페이지)의 필요성

---
+++ src/main/java/com/newlecture/web/Calc3.java
@@ -0,0 +1,91 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/calc3")
+public class Calc3 extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		ServletContext application = req.getServletContext(); // 전역 범위
+		HttpSession session = req.getSession(); // 사용자마다 공간이 다르다.
+		Cookie[] cookies = req.getCookies(); // WebBrowser별 path 범주 공간
+		
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		String v_ = req.getParameter("v");
+		String op = req.getParameter("operator");
+		
+		int v = 0;
+		
+		if(!v_.equals(""))
+			v = Integer.parseInt(v_);
+				
+		if(op.equals("=")) {
+			// int x = (Integer)application.getAttribute("value");
+			// int x = (Integer)session.getAttribute("value");
+			
+			int x = 0;
+			for(Cookie c : cookies) {
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}	
+			}
+			
+			int y = v;
+			// String operator = (String)application.getAttribute("op");
+			// String operator = (String)session.getAttribute("op");
+			
+			String operator = "";
+			for(Cookie c : cookies) {
+				if(c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}	
+			}
+			
+			int result = 0;
+			
+			if(operator.equals("+")) {
+				result = x+y;
+			} else {
+				result = x-y;
+			}
+			
+			resp.getWriter().printf("result is %d\n", result);
+			
+		} else {
+			// application.setAttribute("value", v);
+			// application.setAttribute("op", op);
+			
+			// session.setAttribute("value", v);
+			// session.setAttribute("op", op);
+			
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			
+			valueCookie.setPath("/calc2"); // path 옵션
+			opCookie.setPath("/calc2");
+			
+			valueCookie.setMaxAge(24*60*60); // maxAge 옵션
+			
+			resp.addCookie(valueCookie);
+			resp.addCookie(opCookie);
+			
+			resp.sendRedirect("/calc2.html"); // redirection
+			
+		}
+			
+	}
+}
