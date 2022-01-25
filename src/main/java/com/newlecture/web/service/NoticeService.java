203af32779c9b91aeb593c48c40d720335aa3e0b
yamto <yamto@121.184.173.135>
2022-01-25 09:04:09 +0900

    getPrevNotice 메소드의 SQL 쿼리 작성하고 getNextNotice 메소드 수정하기

--- src/main/java/com/newlecture/web/service/NoticeService.java
+++ src/main/java/com/newlecture/web/service/NoticeService.java
@@ -49,17 +49,21 @@ public class NoticeService {
 	public Notice getPrevNotice(int id) {
 		String sql = "SELECT * FROM NOTICE "
 				+ "WHERE ID = ( "
-				+ "    SELECT ID FROM ( "
-				+ "        SELECT ROWNUM NUM, N.ID "
-				+ "        FROM (SELECT * FROM NOTICE WHERE ID > 3) N " //수정 필요
-				+ "    ) "
-				+ "    WHERE NUM = 1 "
+				+ "    SELECT ID FROM NOTICE "
+				+ "    WHERE REGDATE < (SELECT REGDATE FROM NOTICE WHERE ID=3) "
+				+ "    AND ROWNUM = 1 "
 				+ ")";
 		
 		return null;
 	}
 	
 	public Notice getNextNotice(int id) {
+		String sql = "SELECT * FROM NOTICE "
+				+ "WHERE ID = ( "
+				+ "    SELECT ID FROM (SELECT * FROM NOTICE ORDER BY ID) "
+				+ "    WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID=3) "
+				+ "    AND ROWNUM = 1 "
+				+ ")";
 		
 		return null;
 	}
