528f61b576f82a2ff6a19c898f6a7a62f3e63376
yamto <yamto@118.34.83.127>
2022-01-25 11:59:10 +0900

    getNoticeList의 JDBC 코드 구현하기

--- src/main/java/com/newlecture/web/service/NoticeService.java
+++ src/main/java/com/newlecture/web/service/NoticeService.java
@@ -1,6 +1,8 @@
 package com.newlecture.web.service;
 
+import java.sql.*;
 import java.util.*;
+import java.util.Date;
 
 import com.newlecture.web.entity.*;
 
@@ -15,14 +17,54 @@ public class NoticeService {
 		return getNoticeList("title", "", page);
 	}
 	
-	public List<Notice> getNoticeList(String field, String query, int page) {
+	public List<Notice> getNoticeList(String field/*TITLE, WRITER_ID*/, String query/*A*/, int page) {
+		List<Notice> list = new ArrayList<>();
+		
 		String sql = "SELECT * FROM ("
 				+ "    SELECT ROWNUM NUM, N.* "
-				+ "    FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) N "
+				+ "    FROM (SELECT * FROM NOTICE WHERE "+field+" LIKE ? ORDER BY REGDATE DESC) N "
 				+ ") "
-				+ "WHERE NUM BETWEEN 6 AND 10"; //변수화 필요
+				+ "WHERE NUM BETWEEN ? AND ?";
 		
-		return null;
+		//1, 11, 21, 31 -> an = 1+(page-1)*10
+		//10, 20, 30, 40 -> page*10
+
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "newlec", "rufdml13");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, "%"+query +"%");
+			st.setInt(2, 1+(page-1)*10);
+			st.setInt(3, page*10);
+			
+			ResultSet rs = st.executeQuery();
+
+			while(rs.next()) {
+				int id = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				String writerId = rs.getString("WRITER_ID");
+				Date regdate = rs.getDate("REGDATE");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				Notice notice = new Notice(id, title, writerId, regdate, hit, files, content);
+				
+				list.add(notice);
+			}
+
+			rs.close();
+			st.close();
+			con.close();	
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+				
+		return list;
 	}
 	
 	public int getNoticeCount() {
