3e0d42324e2d1d640da45f053560eca1a46c9370
yamto <yamto@118.34.83.127>
2022-01-25 18:24:50 +0900

    목록 페이지에서 검색 추가하기

--- src/main/java/com/newlecture/web/controller/NoticeListController.java
+++ src/main/java/com/newlecture/web/controller/NoticeListController.java
@@ -14,8 +14,21 @@ import com.newlecture.web.service.*;
 public class NoticeListController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//list?f=title&q=a
+		String field_ = request.getParameter("f");
+		String query_ = request.getParameter("q");
+		
+		String field = "title";
+		if(field_ != null) {
+			field = field_;
+		}
+		String query = "";
+		if(query_ != null) {
+			query = query_;
+		}
+		
 		NoticeService service = new NoticeService();
-		List<Notice> list = service.getNoticeList();
+		List<Notice> list = service.getNoticeList(field, query, 1);
 				
 		request.setAttribute("list", list);
 		
--- src/main/webapp/WEB-INF/view/notice/list.jsp
+++ src/main/webapp/WEB-INF/view/notice/list.jsp
@@ -153,11 +153,11 @@
 						<legend class="hidden">공지사항 검색 필드</legend>
 						<label class="hidden">검색분류</label>
 						<select name="f">
-							<option  value="title">제목</option>
-							<option  value="writerId">작성자</option>
+							<option ${(param.f=="title")?"selected":""} value="title">제목</option>
+							<option ${(param.f=="writer_id")?"selected":""} value="writer_id">작성자</option>
 						</select> 
 						<label class="hidden">검색어</label>
-						<input type="text" name="q" value=""/>
+						<input type="text" name="q" value="${param.q}"/>
 						<input class="btn btn-search" type="submit" value="검색" />
 					</fieldset>
 				</form>
