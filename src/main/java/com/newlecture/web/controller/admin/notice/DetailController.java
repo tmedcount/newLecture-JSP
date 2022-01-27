045ee56ccc63079f823f9998775be26e4aa34bad
yamto <yamto@118.34.83.127>
2022-01-27 19:37:26 +0900

    관리자 공지사항 자세한 페이지 추가

---
+++ src/main/java/com/newlecture/web/controller/admin/notice/DetailController.java
@@ -0,0 +1,27 @@
+package com.newlecture.web.controller.admin.notice;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+import com.newlecture.web.entity.*;
+import com.newlecture.web.service.*;
+
+@WebServlet("/admin/board/notice/detail")
+public class DetailController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		NoticeService service = new NoticeService();
+		Notice notice = service.getNotice(id);
+		request.setAttribute("n", notice);
+		
+		//forward
+		request
+			.getRequestDispatcher("/WEB-INF/view/admin/board/notice/detail.jsp")
+			.forward(request, response);
+	}
+}
