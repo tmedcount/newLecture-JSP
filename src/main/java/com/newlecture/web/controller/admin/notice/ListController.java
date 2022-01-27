45614d8e5cdd2e21ee32abf2cce295eb2f3cfc50
yamto <yamto@118.34.83.127>
2022-01-27 11:39:41 +0900

    admin/notice/list 페이지 추가하기

---
+++ src/main/java/com/newlecture/web/controller/admin/notice/ListController.java
@@ -0,0 +1,48 @@
+package com.newlecture.web.controller.admin.notice;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+import com.newlecture.web.entity.*;
+import com.newlecture.web.service.*;
+
+@WebServlet("/admin/notice/list")
+public class ListController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//list?f=title&q=a
+		String field_ = request.getParameter("f");
+		String query_ = request.getParameter("q");
+		String page_ = request.getParameter("p");
+		
+		
+		String field = "title";
+		if(field_ != null && !field_.equals("")) {
+			field = field_;
+		}
+		String query = "";
+		if(query_ != null && !query_.equals("")) {
+			query = query_;
+		}
+		int page = 1;
+		if(page_ != null && !page_.equals("")) {
+			page = Integer.parseInt(page_);
+		}
+		
+		NoticeService service = new NoticeService();
+		List<NoticeView> list = service.getNoticeList(field, query, page);
+		int count = service.getNoticeCount(field, query);
+				
+		request.setAttribute("list", list);
+		request.setAttribute("count", count);
+		
+		request
+			.getRequestDispatcher("/WEB-INF/view/admin/board/notice/list.jsp")
+			.forward(request, response);
+	}
+}
+
