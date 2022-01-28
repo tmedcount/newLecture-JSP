f79beb3fb5dafdc2317cae64c261bd42870da1f4
yamto <yamto@118.34.83.127>
2022-01-28 13:24:15 +0900

    단일 파일 업로드

--- src/main/java/com/newlecture/web/controller/admin/notice/RegController.java
+++ src/main/java/com/newlecture/web/controller/admin/notice/RegController.java
@@ -35,10 +35,23 @@ public class RegController extends HttpServlet {
 		String isOpen = request.getParameter("open");
 		
 		Part filePart = request.getPart("file");
-		filePart.getInputStream();
+		String fileName = filePart.getSubmittedFileName();
+		InputStream fis = filePart.getInputStream();
 				
 		String realPath = request.getServletContext().getRealPath("/upload");
 		System.out.println(realPath);
+		
+		String filePath = realPath + File.separator + fileName;
+		FileOutputStream fos = new FileOutputStream(filePath);
+		
+		byte[] buf = new byte[1024];
+		int size = 0;
+		while((size = fis.read(buf)) != -1) {
+			fos.write(buf, 0, size);
+		}
+		
+		fos.close();
+		fis.close();
 		
 		boolean pub = false;
 		if(isOpen != null) {
